1281b04fc6f9ab32da1fbe26ffa0235a8809d39d
Vivek <veeveckjeet@gmail.com>
2023-11-15 12:03:29 -0500

    Handle Qr Text and color

--- QR-Code-Generator/index.js
+++ QR-Code-Generator/index.js
@@ -5,3 +5,31 @@ const dark = document.querySelector(".dark");
 const light = document.querySelector(".light");
 const qrAreaCode = document.querySelector("#qr-code");
 const qrText = document.querySelector(".qr-text");
+
+dark.addEventListener("input", handleDarkColor);
+light.addEventListener("input", handleLightColor);
+qrText.addEventListener("input", handleQRText);
+sizes.addEventListener("change", sizeHandler);
+shareBtn.addEventListener("click", shareHandler);
+
+const defaultUrl = "https://www.google.com";
+let colorLight = "#fff",
+  colorDark = "#000",
+  text = defaultUrl,
+  size = 300;
+
+function handleDarkColor(e) {
+  colorDark = e.target.value;
+  generateQRCode();
+}
+function handleLightColor(e) {
+  colorLight = e.target.value;
+  generateQRCode();
+}
+function handleQRText(e) {
+  const value = e.target.value;
+  text = value;
+  if (!value) text = defaultUrl;
+  generateQRCode();
+}
+async function generateQRCode() {}
