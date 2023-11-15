a53978a73de5a15be0c8910e1e6a6b9f7684be4e
Vivek <veeveckjeet@gmail.com>
2023-11-15 11:39:34 -0500

    Initial JS

---
+++ QR-Code-Generator/index.js
@@ -0,0 +1,7 @@
+const download = document.querySelector(".download");
+const shareBtn = document.querySelector(".share-btn");
+const sizes = document.querySelector(".sizes");
+const dark = document.querySelector(".dark");
+const light = document.querySelector(".light");
+const qrAreaCode = document.querySelector("#qr-code");
+const qrText = document.querySelector(".qr-text");
