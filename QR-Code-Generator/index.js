8063453bca16121695600cc647e70fd3d837cd57
Vivek <veeveckjeet@gmail.com>
2023-11-16 11:46:27 -0500

    QR Code generator

--- QR-Code-Generator/index.js
+++ QR-Code-Generator/index.js
@@ -32,4 +32,48 @@ function handleQRText(e) {
   if (!value) text = defaultUrl;
   generateQRCode();
 }
-async function generateQRCode() {}
+async function generateQRCode() {
+  qrAreaCode.innerHTML = "";
+  new QRCode("qr-code", {
+    text,
+    height: size,
+    width: size,
+    colorLight,
+    colorDark,
+  });
+  download.href = await resolveDataUrl();
+}
+async function shareHandler() {
+  setTimeout(async () => {
+    try {
+      const baseUrl = await resolveDataUrl();
+      const blob = await (await fetch(baseUrl)).blob();
+      const file = new File([blob], "QRCode.png", {
+        type: blob.type,
+      });
+      await navigator.share({
+        files: [file],
+        title: text,
+      });
+    } catch (error) {
+      alert("Browser doesn't support sharing.");
+    }
+  }, 100);
+}
+function sizeHandler(e) {
+  size = e.target.value;
+  generateQRCode();
+}
+function resolveDataUrl() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const img = document.querySelector("#qr-code img");
+      if (img.currentSrc) {
+        resolve(img.currentSrc);
+        return;
+      }
+      const canvas = document.querySelector("canvas");
+      resolve(canvas.toDataURL());
+    }, 50);
+  });
+}
