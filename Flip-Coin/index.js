1b37b09faaefe64e3dc1546677c4b665a4cadab4
Vivek <veeveckjeet@gmail.com>
2023-11-18 19:18:35 -0500

    Reset n Update State

--- Flip-Coin/index.js
+++ Flip-Coin/index.js
@@ -3,3 +3,20 @@ let tails = 0;
 let coin = document.querySelector(".coins");
 let flipBtn = document.querySelector("#flip-button");
 let resetBtn = document.querySelector("#reset-button");
+let headCount = document.querySelector("#heads-count");
+let tailCount = document.querySelector("#tails-count");
+
+flipBtn.addEventListener("click", () => {});
+resetBtn.addEventListener("click", () => {});
+
+//Update State of head n tails
+function updateState() {
+  headCount.textContent = `Heads :${heads}`;
+  tailCount.textContent = `Tails :${tails}`;
+}
+//Reset State
+resetBtn.addEventListener("click", () => {
+  heads = 0;
+  tails = 0;
+  updateState();
+});
