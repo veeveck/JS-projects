8a8ecff1aea3cd5d705dca8518476ff108513458
Vivek <veeveckjeet@gmail.com>
2023-11-18 19:17:45 -0500

    Update n Reset State

---
+++ Flip-Coin/index.js
@@ -0,0 +1,5 @@
+let heads = 0;
+let tails = 0;
+let coin = document.querySelector(".coins");
+let flipBtn = document.querySelector("#flip-button");
+let resetBtn = document.querySelector("#reset-button");
