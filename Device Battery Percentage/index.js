d8fef0cf5edb530ffcde6c666cfab4fd666c586a
Vivek <veeveckjeet@gmail.com>
2023-11-24 11:03:33 -0500

    css styles and more test cases charging added

--- Device Battery Percentage/index.js
+++ Device Battery Percentage/index.js
@@ -12,10 +12,25 @@ function initBattery() {
       if (level == 100) {
         batteryStatus.innerHTML = `Battery Full <i class="ri-battery-2-fill green-color"></i>`;
         batteryLiquid.style.height = "101%";
-      } else if ((level <= 20) & !batt.charging) {
+      } else if ((level <= 20) & !battery.charging) {
         batteryStatus.innerHTML = `Low Charge <i class="ri-plug-line animated-red animated-red"></i>`;
-      } else if (batt.charging) {
+      } else if (battery.charging) {
         batteryStatus.innerHTML = `Charging ... <i class="ri-flashlight-line animated-green"></i>`;
+      }
+      if (level <= 20) {
+        batteryLiquid.classList.add("gradient-color-red");
+        batteryLiquid.classList.remove(
+          "gradient-color-green",
+          "gradient-color-orange",
+          "gradient-color-yellow"
+        );
+      } else if (level <= 48) {
+        batteryLiquid.classList.add("gradient-color-orange");
+        batteryLiquid.classList.remove(
+          "gradient-color-green",
+          "gradient-color-red",
+          "gradient-color-yellow"
+        );
       }
     };
     updateBattery();
@@ -23,7 +38,7 @@ function initBattery() {
       updateBattery();
     });
     battery.addEventListener("levelchange", () => {
-      updateBattery;
+      updateBattery();
     });
   });
 }
