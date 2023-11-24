9d4d7e475187dec7e42d44644a8a1d1cc29d3e81
Vivek <veeveckjeet@gmail.com>
2023-11-24 10:26:48 -0500

    Initial JS code

---
+++ Device Battery Percentage/index.js
@@ -0,0 +1,7 @@
+initBattery();
+
+function initBattery() {
+  const batteryLiquid = document.querySelector(".battery-liquid");
+  const batteryStatus = document.querySelector(".battery-status");
+  const batteryPercentage = document.querySelector(".battery-percent");
+}
