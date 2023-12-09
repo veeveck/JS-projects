df36636bad80274a581e76e7cdfe6a9dfb4fad03
Vivek <veeveckjeet@gmail.com>
2023-12-09 15:49:26 -0500

    css and some error fix

---
+++ Dynamic-Progress-Bar/index.js
@@ -0,0 +1,5 @@
+const progress = document.querySelector(".progress-done");
+const val = document.querySelector(".input-value");
+const maxVal = document.querySelector(".max-value");
+let finalVal = 0;
+let finalMaxVal = 0;
