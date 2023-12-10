9ca3171dec219bfc2d4fe25dd2a31e1f2f2eb2e3
Vivek <veeveckjeet@gmail.com>
2023-12-10 10:46:22 -0500

    change Width JS

--- Dynamic-Progress-Bar/index.js
+++ Dynamic-Progress-Bar/index.js
@@ -3,3 +3,16 @@ const val = document.querySelector(".input-value");
 const maxVal = document.querySelector(".max-value");
 let finalVal = 0;
 let finalMaxVal = 0;
+
+function changeWidth() {
+  progress.style.width = `${(finalVal / finalMaxVal) * 100}%`;
+  progress.innerText = `${Math.ceil((finalVal / finalMaxVal) * 100)}%`;
+}
+val.addEventListener("keyup", function () {
+  finalVal = parseInt(val.value, 10);
+  changeWidth();
+});
+maxVal.addEventListener("keyup", function () {
+  finalMaxVal = parseInt(maxVal.value, 10);
+  changeWidth();
+});
